5c2af335a234787a84b1dbf7a21f0a51e9ab1e12
Pascal <pascal.pestiauxplaposte.net>
2026-01-29 16:13:28 +0100

    Test du pipeline 2Ã

--- src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
+++ src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TourguideApplication {
 
-	//Test du pipeline
 	public static void main(String[] args) {
 		SpringApplication.run(TourguideApplication.class, args);
 	}
