3aaf3d49f33744e032d85e946bbc1f495ddf6d70
Pascal <pascal.pestiauxplaposte.net>
2026-03-14 13:54:19 +0100

    Correction nom de classe

--- src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
+++ src/main/java/com/openclassrooms/tourguide/TourguideApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TourguideApplication {
+public class TourGuideApplication {
 
 	//Test du pipeline
 	public static void main(String[] args) {
-		SpringApplication.run(TourguideApplication.class, args);
+		SpringApplication.run(TourGuideApplication.class, args);
 	}
 
 }
