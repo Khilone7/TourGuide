ba3a0db8787a09b0ef36daa7cd96ce8468d257f4
Pascal <pascal.pestiauxplaposte.net>
2026-03-16 14:02:02 +0100

    Test

--- src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
+++ src/main/java/com/openclassrooms/tourguide/TourGuideApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TourGuideApplication {
 
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(TourGuideApplication.class, args);
 	}
